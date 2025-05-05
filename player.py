f2563651af2fdf7f86dc6290f510a962f2b27cec
DAYFA1R <im@dayfair.ca>
2025-05-05 12:09:05 -0400

    added shot limiter

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
   def __init__(self, x, y):
     super().__init__(x, y, PLAYER_RADIUS)
     self.rotation = 0
+    self.timer = 0
 
   # in the player class
   def triangle(self):
@@ -28,12 +29,14 @@ class Player(CircleShape):
     self.position += forward * PLAYER_SPEED * dt
     
   def shoot(self, dt):
-    print("shooting")
-    x, y = self.position
-    shot = Shot(x, y, SHOT_RADIUS)
-    shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+    if self.timer <= 0:
+      self.timer = PLAYER_SHOOT_COOLDOWN
+      x, y = self.position
+      shot = Shot(x, y, SHOT_RADIUS)
+      shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
   
   def update(self, dt):
+    self.timer -= dt
     keys = pygame.key.get_pressed()
     
     if keys[pygame.K_a]:
